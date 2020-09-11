78ab1499c6531625be38ba0a8490d8fa9eeba2e9
Midhun <midhun.connect@gmail.com>
2020-09-11 15:53:10 +0530

    First commit.
    Creating a Netflix eureka Server and Client Mechanism for handling the
    Micro-service registration and discovering the service.

---
+++ Springcloud-eureka-server/src/test/java/com/pe/springcloud/eureka/server/SpringcloudEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pe.springcloud.eureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
